898ff8a9179d054a2dc0a1a5ef7c607472a2515c
erikvs <89622099+erikvs@users.noreply.github.com>
2023-03-04 13:54:38 +0100

    Nav.js
    Added nav component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './StyleFolder/main.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
@@ -6,19 +7,12 @@ function App() {
       <body>
 
 <header>
-    <h1>Ressursarktiv</h1>
+    <h1>Ressursarkiv</h1>
 </header>
 
 
 
-<nav class="container">
-  <button class="tablinks" id="HTMLKnapp">HTML</button>
-  <button class="tablinks" id="CSSKnapp">CSS</button>
-  <button class="tablinks" id="JavaKnapp">JavaScript</button>
-  <button class="tablinks" id="ReactKnapp">React</button>
-  <button class="tablinks" id="SanityKnapp">Sanity and headless CMS</button>
-
-</nav>
+<Nav />
 
 <main>
  
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+export default function Nav(){
+    return (
+        <nav>
+            <a href="html.html">HTML</a>
+            <a href="css.html">CSS</a>
+            <a href="javascript.html">JavaScript</a>
+            <a href="React.html">React</a>
+            <a href="sanity-and-headless-cms.html">Sanity and headless CMS</a>
+        </nav>
+    )
+}
